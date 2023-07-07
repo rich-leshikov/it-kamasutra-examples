e9f39ace8b149f1478386cdbe43caf0d6baab717
Richard Leshik <richardleshik@gmail.com>
2023-07-07 12:08:09 +0300

    add analog clock mode

--- src/components/Clock/Clock.stories.tsx
+++ src/components/Clock/Clock.stories.tsx
@@ -1,14 +1,13 @@
-import {Clock} from './Clock';
-import {Story} from '@storybook/react';
+import {Clock} from './Clock'
+import {Story} from '@storybook/react'
+
 
 export default {
   title: 'Clock',
   component: Clock,
 }
 
-const Template: Story = (args) => <Clock {...args}/>
+const Template: Story = () => <Clock/>
 
 export const DefaultMode = Template.bind({})
-DefaultMode.args = {
-  time: '00:00:00'
-}
+
--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,12 +1,14 @@
 import React, {useEffect, useRef, useState} from 'react'
+import styles from './AnalogClock.module.css'
 
 
 export const Clock = React.memo(() => {
+  const [mode, setMode] = useState<'digital' | 'analog'>('digital')
   const [time, setTime] = useState<string>('00:00:00')
   const [isTicking, setTicking] = useState<boolean>(false)
   const [date, setDate] = useState<Date>(new Date())
 
-  const digitalClockStyle = {fontSize: '24px', color: '#646464', fontFamily: 'impact'}
+  const digitalClockStyle = {fontSize: '64px', color: '#646464', fontFamily: 'impact'}
 
   const timerRef = useRef<ReturnType<typeof setInterval>>()
 
@@ -23,14 +25,57 @@ export const Clock = React.memo(() => {
       }).format(date)
 
       setTime(stringTime)
+
       return () => {
         clearInterval(timerRef.current)
       }
     }
-
   }, [isTicking, date])
 
+  const onChangeMode = () => mode === 'digital' ? setMode('analog') : setMode('digital')
   const onClickCondition = () => !isTicking ? setTicking(true) : setTicking(false)
 
-  return <div style={digitalClockStyle} onClick={onClickCondition}>{time}</div>
+  return (
+    <>
+      <button onClick={onChangeMode}>Change mode</button>
+      <div onClick={onClickCondition}>
+        {
+          mode === 'digital'
+            ? <div style={digitalClockStyle}>{time}</div>
+            : <div className={styles.clock}>
+              <div
+                className={styles.hourHand}
+                style={{
+                  transform: `rotateZ(${date.getHours() * 30}deg)`
+                }}
+              />
+              <div
+                className={styles.minHand}
+                style={{
+                  transform: `rotateZ(${date.getMinutes() * 6}deg)`
+                }}
+              />
+              <div
+                className={styles.secHand}
+                style={{
+                  transform: `rotateZ(${date.getSeconds() * 6}deg)`
+                }}
+              />
+              <span className={styles.twelve}>12</span>
+              <span className={styles.one}>1</span>
+              <span className={styles.two}>2</span>
+              <span className={styles.three}>3</span>
+              <span className={styles.four}>4</span>
+              <span className={styles.five}>5</span>
+              <span className={styles.six}>6</span>
+              <span className={styles.seven}>7</span>
+              <span className={styles.eight}>8</span>
+              <span className={styles.nine}>9</span>
+              <span className={styles.ten}>10</span>
+              <span className={styles.eleven}>11</span>
+            </div>
+        }
+      </div>
+    </>
+  )
 })
